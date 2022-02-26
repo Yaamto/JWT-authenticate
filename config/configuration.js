35256fca8a06d01b4a023b3339bd583cf135c183
yaamto <bilou.bout@gmail.com>
2022-02-26 12:26:45 +0100

    MongoDB connect hide + gitignore

--- config/configuration.js
+++ config/configuration.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://Bilal:bilal@cluster0.ivelk.mongodb.net/test")
+    process.env.DB_CONNECT)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
